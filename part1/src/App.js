64d8dc79c1439df2d850ed1882aa42ad45c5b37e
Artem Ivanov <arti.ivnv@gmail.com>
2023-01-06 21:36:21 +0100

    Final push for a Part 1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,60 +1,111 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
+import { useState } from "react";
 
-const Content = (props) => {
+const StatisticsLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td> {value}</td>
+  </tr>
+);
+
+const Statistics = (props) => {
+  let total = 0;
+  let average = 0;
+  let positivePercent = 0;
+
+  total = props.good + props.neutral + props.bad;
+  average = ((props.good - props.bad) / total).toFixed(1);
+  positivePercent = ((props.good / total) * 100).toFixed(1) + " %";
+
+  if (total === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={props.good} />
+          <StatisticsLine text="neutral" value={props.neutral} />
+          <StatisticsLine text="bad" value={props.bad} />
+          <StatisticsLine text="all" value={total} />
+          <StatisticsLine text="average" value={average} />
+          <StatisticsLine text="positive" value={positivePercent} />
+        </tbody>
+      </table>
     </div>
   );
 };
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-};
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
-const Total = (props) => {
-  return (
-    <p>
-      {" "}
-      Number of exercises{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
-  );
-};
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+  const handleRand = () => setSelected(Math.floor(Math.random() * max));
+  const handleVote = () => {
+    const copy = [...votes];
+    copy[selected]++;
+    setVotes(copy);
   };
+
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  ];
+
+  let max = anecdotes.length - 1;
+
+  let highest = 0;
+  let highestA = 0;
+
+  const [votes, setVotes] = useState(Array(max).fill(0));
+
+  for (let i = 0; i < votes.length; i++) {
+    if (votes[i] > highest) {
+      highest = votes[i];
+      highestA = i;
+    }
+  }
+
+  console.log(highest);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>give a feedback</h1>
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      <br />
+      <div>
+        {anecdotes[selected]} <br /> has {votes[selected]} votes
+      </div>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleRand} text="next anecdote" />
+
+      <h1>Anecdote with most votes</h1>
+      <div>
+        {anecdotes[highestA]} <br /> has {highest} votes
+      </div>
     </div>
   );
 };
